23a130620e3dd8d3e29d06a24f906579ac704450
Hiss0 <70361528+Hiss0@users.noreply.github.com>
2020-09-11 20:21:18 +0900

    .

---
+++ sketch_200911b/sketch_200911b.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 500);
+  fill(200, 100, 100);
+  ellipse(100, 100, 100, 100);
+  fill(100, 100, 200);
+  ellipse(300, 100, 100, 100);
+  fill(100, 200, 100);
+  ellipse(100, 300, 100, 100);
+  fill(200, 100, 200);
+  ellipse(300, 300, 100, 100);
+  fill(200, 200, 200);
+  ellipse(200, 200, 100, 100);
+}
+
+
+function draw() {
+
+}
