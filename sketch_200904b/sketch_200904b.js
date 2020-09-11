9012a43c724707a42457c18a561d588aa0f8220a
Hiss0 <70361528+Hiss0@users.noreply.github.com>
2020-09-11 14:32:53 +0900

    wow

---
+++ sketch_200904b/sketch_200904b.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500, 500);
+  background(209);
+  point(250, 250);
+  line(0, 0, 500, 500);
+  ellipse(250, 250, 300, 300);
+  triangle(250, 100, 100, 250, 400, 250);
+  quad(250, 100, 100, 250, 250, 400, 400, 250);
+  
+}
+
+
+function draw() {
+
+}
